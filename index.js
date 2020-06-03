72143f20963eebf8d51a9124c234a175db8a198b
Nicholas Jamieson <nicholas@cartant.com>
2020-06-03 11:31:00 +1000

    chore: Add deprecation.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugin: ["etc"],
   rules: {
+    "etc/deprecation": ["warning"],
     "etc/no-assign-mutated-array": ["error"],
     "etc/no-unused-declaration": ["error"],
     "etc/throw-error": ["error"]
