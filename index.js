cd778dfa25f33a6fc7e7e3590b571db6f562d7b8
Nicholas Jamieson <nicholas@cartant.com>
2020-10-01 14:41:41 +1000

    chore: Add no-internal; use renamed no-deprecated.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = {
   plugins: ["etc"],
   rules: {
-    "etc/deprecation": ["warn"],
     "etc/no-array-foreach": ["error"],
     "etc/no-assign-mutated-array": ["error"],
+    "etc/no-deprecated": ["warn"],
     "etc/no-implicit-any-catch": ["error"],
+    "etc/no-internal": ["warn"],
     "etc/throw-error": ["error"],
   },
 };
