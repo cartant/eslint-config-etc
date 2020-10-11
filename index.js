378802555da9287264b518faf29c62b4549b559a
Nicholas Jamieson <nicholas@cartant.com>
2020-10-12 09:39:38 +1000

    chore: Switch to no-foreach.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
   plugins: ["etc"],
   rules: {
-    "etc/no-array-foreach": ["error"],
     "etc/no-assign-mutated-array": ["error"],
     "etc/no-deprecated": ["warn"],
+    "etc/no-foreach": ["error"],
     "etc/no-implicit-any-catch": ["error"],
-    "etc/no-internal": ["warn"],
+    "etc/no-internal": ["error"],
     "etc/throw-error": ["error"],
   },
 };
